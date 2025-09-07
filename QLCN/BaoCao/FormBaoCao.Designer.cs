931bdd71ba82cc3499898caec4b899ebf83ea299
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-07 22:33:55 +0700

    form bao cao

--- QLCN/BaoCao/FormBaoCao.Designer.cs
+++ QLCN/BaoCao/FormBaoCao.Designer.cs
@@ -26,12 +26,13 @@
             btnView = new Button();
             btnExport = new Button();
             dgvBaoCao = new DataGridView();
+            title = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvBaoCao).BeginInit();
             SuspendLayout();
             // 
             // lblLoai
             // 
-            lblLoai.Location = new Point(30, 30);
+            lblLoai.Location = new Point(36, 92);
             lblLoai.Name = "lblLoai";
             lblLoai.Size = new Size(121, 31);
             lblLoai.TabIndex = 0;
@@ -39,24 +40,24 @@
             // 
             // lblFrom
             // 
-            lblFrom.Location = new Point(30, 72);
+            lblFrom.Location = new Point(36, 136);
             lblFrom.Name = "lblFrom";
-            lblFrom.Size = new Size(121, 23);
+            lblFrom.Size = new Size(121, 31);
             lblFrom.TabIndex = 2;
             lblFrom.Text = "Từ ngày:";
             // 
             // lblTo
             // 
-            lblTo.Location = new Point(30, 112);
+            lblTo.Location = new Point(36, 176);
             lblTo.Name = "lblTo";
-            lblTo.Size = new Size(100, 23);
+            lblTo.Size = new Size(100, 31);
             lblTo.TabIndex = 4;
             lblTo.Text = "Đến ngày:";
             // 
             // cboLoaiBaoCao
             // 
             cboLoaiBaoCao.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboLoaiBaoCao.Location = new Point(157, 20);
+            cboLoaiBaoCao.Location = new Point(163, 92);
             cboLoaiBaoCao.Name = "cboLoaiBaoCao";
             cboLoaiBaoCao.Size = new Size(200, 33);
             cboLoaiBaoCao.TabIndex = 1;
@@ -64,7 +65,7 @@
             // dtpFrom
             // 
             dtpFrom.Format = DateTimePickerFormat.Short;
-            dtpFrom.Location = new Point(157, 64);
+            dtpFrom.Location = new Point(163, 136);
             dtpFrom.Name = "dtpFrom";
             dtpFrom.Size = new Size(200, 31);
             dtpFrom.TabIndex = 3;
@@ -72,14 +73,14 @@
             // dtpTo
             // 
             dtpTo.Format = DateTimePickerFormat.Short;
-            dtpTo.Location = new Point(157, 104);
+            dtpTo.Location = new Point(163, 176);
             dtpTo.Name = "dtpTo";
             dtpTo.Size = new Size(200, 31);
             dtpTo.TabIndex = 5;
             // 
             // btnView
             // 
-            btnView.Location = new Point(403, 20);
+            btnView.Location = new Point(393, 92);
             btnView.Name = "btnView";
             btnView.Size = new Size(129, 33);
             btnView.TabIndex = 6;
@@ -88,7 +89,7 @@
             // 
             // btnExport
             // 
-            btnExport.Location = new Point(403, 64);
+            btnExport.Location = new Point(393, 136);
             btnExport.Name = "btnExport";
             btnExport.Size = new Size(129, 31);
             btnExport.TabIndex = 7;
@@ -98,15 +99,26 @@
             // dgvBaoCao
             // 
             dgvBaoCao.ColumnHeadersHeight = 34;
-            dgvBaoCao.Location = new Point(30, 161);
+            dgvBaoCao.Location = new Point(36, 226);
             dgvBaoCao.Name = "dgvBaoCao";
             dgvBaoCao.RowHeadersWidth = 62;
-            dgvBaoCao.Size = new Size(502, 277);
+            dgvBaoCao.Size = new Size(486, 335);
             dgvBaoCao.TabIndex = 8;
+            // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            title.Location = new Point(200, 30);
+            title.Name = "title";
+            title.Size = new Size(139, 45);
+            title.TabIndex = 9;
+            title.Text = "Báo cáo";
             // 
             // FormBaoCao
             // 
-            ClientSize = new Size(575, 500);
+            ClientSize = new Size(571, 591);
+            Controls.Add(title);
             Controls.Add(lblLoai);
             Controls.Add(cboLoaiBaoCao);
             Controls.Add(lblFrom);
@@ -122,6 +134,8 @@
             Load += FormBaoCao_Load;
             ((System.ComponentModel.ISupportInitialize)dgvBaoCao).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
+        private Label title;
     }
 }
