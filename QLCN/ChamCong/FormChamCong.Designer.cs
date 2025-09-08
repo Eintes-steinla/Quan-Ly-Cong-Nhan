0b10eae63877d1a305913d4e129eb3702efa06f4
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2025-09-08 07:13:03 +0700

    form ChamCOng

--- QLCN/ChamCong/FormChamCong.Designer.cs
+++ QLCN/ChamCong/FormChamCong.Designer.cs
@@ -28,21 +28,22 @@
             btnEdit = new Button();
             btnDelete = new Button();
             btnRefresh = new Button();
+            title = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvChamCong).BeginInit();
             SuspendLayout();
             // 
             // dgvChamCong
             // 
             dgvChamCong.ColumnHeadersHeight = 34;
-            dgvChamCong.Location = new Point(30, 200);
+            dgvChamCong.Location = new Point(31, 252);
             dgvChamCong.Name = "dgvChamCong";
             dgvChamCong.RowHeadersWidth = 62;
-            dgvChamCong.Size = new Size(569, 250);
+            dgvChamCong.Size = new Size(569, 322);
             dgvChamCong.TabIndex = 0;
             // 
             // lblMaCN
             // 
-            lblMaCN.Location = new Point(30, 30);
+            lblMaCN.Location = new Point(31, 112);
             lblMaCN.Name = "lblMaCN";
             lblMaCN.Size = new Size(100, 23);
             lblMaCN.TabIndex = 1;
@@ -50,7 +51,7 @@
             // 
             // lblNgay
             // 
-            lblNgay.Location = new Point(30, 70);
+            lblNgay.Location = new Point(31, 152);
             lblNgay.Name = "lblNgay";
             lblNgay.Size = new Size(100, 23);
             lblNgay.TabIndex = 2;
@@ -58,7 +59,7 @@
             // 
             // lblTrangThai
             // 
-            lblTrangThai.Location = new Point(30, 110);
+            lblTrangThai.Location = new Point(31, 192);
             lblTrangThai.Name = "lblTrangThai";
             lblTrangThai.Size = new Size(100, 23);
             lblTrangThai.TabIndex = 3;
@@ -67,7 +68,7 @@
             // cbMaCN
             // 
             cbMaCN.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbMaCN.Location = new Point(136, 20);
+            cbMaCN.Location = new Point(137, 102);
             cbMaCN.Name = "cbMaCN";
             cbMaCN.Size = new Size(180, 33);
             cbMaCN.TabIndex = 4;
@@ -76,7 +77,7 @@
             // 
             cbTrangThai.DropDownStyle = ComboBoxStyle.DropDownList;
             cbTrangThai.Items.AddRange(new object[] { "Đi làm", "Nghỉ phép", "Nghỉ không phép", "Làm thêm" });
-            cbTrangThai.Location = new Point(136, 107);
+            cbTrangThai.Location = new Point(137, 189);
             cbTrangThai.Name = "cbTrangThai";
             cbTrangThai.Size = new Size(180, 33);
             cbTrangThai.TabIndex = 6;
@@ -84,7 +85,7 @@
             // dtpNgay
             // 
             dtpNgay.Format = DateTimePickerFormat.Short;
-            dtpNgay.Location = new Point(136, 65);
+            dtpNgay.Location = new Point(137, 147);
             dtpNgay.Name = "dtpNgay";
             dtpNgay.Size = new Size(180, 31);
             dtpNgay.TabIndex = 5;
@@ -92,7 +93,7 @@
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(371, 18);
+            btnAdd.Location = new Point(372, 100);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(102, 35);
             btnAdd.TabIndex = 7;
@@ -101,7 +102,7 @@
             // 
             // btnEdit
             // 
-            btnEdit.Location = new Point(497, 18);
+            btnEdit.Location = new Point(498, 100);
             btnEdit.Name = "btnEdit";
             btnEdit.Size = new Size(102, 35);
             btnEdit.TabIndex = 8;
@@ -110,7 +111,7 @@
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(371, 65);
+            btnDelete.Location = new Point(372, 147);
             btnDelete.Name = "btnDelete";
             btnDelete.Size = new Size(102, 31);
             btnDelete.TabIndex = 9;
@@ -119,16 +120,27 @@
             // 
             // btnRefresh
             // 
-            btnRefresh.Location = new Point(497, 65);
+            btnRefresh.Location = new Point(498, 147);
             btnRefresh.Name = "btnRefresh";
             btnRefresh.Size = new Size(102, 31);
             btnRefresh.TabIndex = 10;
             btnRefresh.Text = "Làm mới";
             btnRefresh.Click += btnRefresh_Click;
             // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            title.Location = new Point(156, 27);
+            title.Name = "title";
+            title.Size = new Size(306, 45);
+            title.TabIndex = 11;
+            title.Text = "Quản lý chấm công";
+            // 
             // FormChamCong
             // 
-            ClientSize = new Size(636, 500);
+            ClientSize = new Size(636, 605);
+            Controls.Add(title);
             Controls.Add(dgvChamCong);
             Controls.Add(lblMaCN);
             Controls.Add(lblNgay);
@@ -146,6 +158,8 @@
             Load += FormChamCong_Load;
             ((System.ComponentModel.ISupportInitialize)dgvChamCong).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
+        private Label title;
     }
 }
